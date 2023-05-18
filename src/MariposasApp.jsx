c6d8d2da06754b85c99831926a340eb28dd16229
Roman Arenas <roman.ea3232@gmail.com>
2023-05-18 13:23:43 -0300

    Mariposas

---
+++ src/MariposasApp.jsx
@@ -0,0 +1,7 @@
+
+
+export const MariposasApp = () => {
+  return (
+    <div>MariposasApp</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {MariposasApp} from './MariposasApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <MariposasApp/>
+  </React.StrictMode>,
+)
